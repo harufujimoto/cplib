04e5b2d14803ec354c9feddaf89de42edb777f17
harufujimoto <fujimoto-haruki-dn@ynu.jp>
2020-08-24 10:43:46 +0900

    0824_1

---
+++ macro.cpp
@@ -0,0 +1,4 @@
+#define REP(i,n)
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
---
+++ template.cpp
@@ -0,0 +1,12 @@
+// author : lynmisakura
+#include<bits/stdc++.h>
+using namespace std;
+
+#define REP(i,n) for(int i = 0;i < n;i++)
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(),(x).end()
+
+template<class T> bool chmax(T& a,T b){ if(a > b){ a = b; return true; }else return false; }
+template<class T> bool chmin(T& a,T b){ if(a < b){ a = b; return true; }else return false; }

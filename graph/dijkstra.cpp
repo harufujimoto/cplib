d7a17abaaa0428df37e173d30d341c5fd1a02249
harufujimoto <fujimoto-haruki-dn@ynu.jp>
2020-08-24 19:10:17 +0900

    dijkstra

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -5,9 +5,11 @@ template<class T> struct Dijkstra{
   using P = pair<T,int>;
   Graph<T> G;
   vector<T> d;
+  vector<int> prev;
   Dijkstra(Graph<T>& G):G(G){}
   void solve(int s){
     d.assign(G.n,INF);
+    prev.assign(G.n,-1);
     d[s] = 0;
     priority_queue<P,vector<P>,greater<P> > que;
     que.push(P(0,s));
@@ -19,10 +21,20 @@ template<class T> struct Dijkstra{
         Edge<T>& e = G[v][i];
         if(d[e.to] > d[v] + e.cost){
           d[e.to] = d[v] + e.cost;
+          prev[e.to] = v;
           que.push(P(d[e.to] , e.to));
         }
       }
     }
+  }
+  bool reachable(int t){
+    return d[t] != INF;
+  }
+  vector<int> get_path(int t){
+    vector<int> path;
+    for(;t != -1;t = prev[t]){ path.push_back(t); }
+    reverse(path.begin(),path.end());
+    return path;
   }
   T& operator[](int i){return d[i];}
 };
